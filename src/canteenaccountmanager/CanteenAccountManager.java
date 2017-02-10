76f98bd522dcfe61a517c54a1af75d3fdd19a933
ybe <ybe@Y.B.E>
2017-02-10 13:04:37 +0300

    Desing basics have been decided and done a few.

--- src/canteenaccountmanager/CanteenAccountManager.java
+++ src/canteenaccountmanager/CanteenAccountManager.java
@@ -15,6 +15,7 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 /**
@@ -23,10 +24,16 @@ import javafx.stage.Stage;
  */
 public class CanteenAccountManager extends Application {
 
+    Image imgBora = new Image("img//programmer_vs_hacker.jpg");
+    Image imgKaan = new Image("img//chemistry-cat.jpg");
+    Image imgDebtButton = new Image("img//debt_icon.jpg");
+    Button debtButton = new Button();
+        
     @Override
     public void start(Stage stage) {
         
         Scene scene = LoginDesign();
+        scene.getStylesheets().add("canteenaccountmanager//Design.css");
         stage.setTitle("Canteen Account Manager");
         stage.setScene(scene);
         stage.show();
@@ -37,35 +44,50 @@ public class CanteenAccountManager extends Application {
         launch(args);
     }
     private Scene LoginDesign() {        
-        Image imgBora = new Image("img//programmer_vs_hacker.jpg");
-        Image imgKaan = new Image("img//chemistry-cat.jpg");
-        System.out.println(imgBora.getHeight());
-        Button deptButton = new Button("Hesaplar");
+        
+        debtButton.setText("Hesaplar");
+        debtButton.setGraphic(new ImageView(imgDebtButton));
+        debtButton.setGraphicTextGap(5.0);
+        debtButton.applyCss();
+        debtButton.setId("shiny-orange");
+        
         Button addAccountButton = new Button("Kullanıcı Ekle");
-        deptButton.setTooltip(new Tooltip("Borç Hesaplarını Görüntülemek ve Düzenlemek için Tıklayınız"));
+        debtButton.setTooltip(new Tooltip("Borç Hesaplarını Görüntülemek ve Düzenlemek için Tıklayınız"));
         addAccountButton.setTooltip(new Tooltip("Yeni Bir Kullanıcı Eklemek İçin Tıklayınız"));
         
-        Button boraButton = new Button(null, new ImageView(imgBora));
-        Button kaanButton = new Button(null, new ImageView(imgKaan));
+        HBox hboxDept = new HBox(0);
+        hboxDept.setAlignment(Pos.BASELINE_RIGHT);
+        hboxDept.getChildren().add(debtButton);
         
+        ImageView ivB = new ImageView(imgBora);
+        ivB.setFitHeight(200);
+        ivB.setFitWidth(200);
         
-        boraButton.setPrefWidth(175);
-        kaanButton.setPrefHeight(175);
-        kaanButton.setPrefWidth(175);
+        ImageView ivK = new ImageView(imgKaan);
+        ivK.setFitHeight(200);
+        ivK.setFitWidth(200);
         
-        HBox hbox = new HBox(1);
-        hbox.setSpacing(15);
-        hbox.alignmentProperty().set(Pos.CENTER);
-        hbox.getChildren().addAll(boraButton, kaanButton);
+        Button boraButton = new Button(null, ivB);
+        boraButton.applyCss();
+        boraButton.setId("shiny-orange");
+        Button kaanButton = new Button(null, ivK);
+        kaanButton.applyCss();
+        kaanButton.setId("shiny-orange");
+        
+        HBox hboxUser = new HBox(1);
+        hboxUser.setSpacing(25);
+        hboxUser.alignmentProperty().set(Pos.CENTER);
+        hboxUser.getChildren().addAll(boraButton, kaanButton);
         
         VBox vbox = new VBox(2);
+        vbox.setSpacing(25);
         vbox.setAlignment(Pos.CENTER);
-        vbox.getChildren().addAll(deptButton, hbox, addAccountButton);
+        vbox.getChildren().addAll(hboxDept, hboxUser, addAccountButton);
         
         BorderPane root = new BorderPane();
         root.setCenter(vbox);
         
-        Scene S1 = new Scene(root, 600, 400);
+        Scene S1 = new Scene(root, 600, 450);
         return S1;
     }
 }
