6461341c74cf45195aa8ce3687956951279a278e
ybe <ybe@Y.B.E>
2017-02-23 14:17:23 +0300

    Worked on ListView on DeptPage

--- src/canteenaccountmanager/CanteenAccountManager.java
+++ src/canteenaccountmanager/CanteenAccountManager.java
@@ -6,15 +6,22 @@
 package canteenaccountmanager;
 
 import javafx.application.Application;
+import static javafx.application.Application.launch;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 import javafx.scene.control.Tooltip;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
@@ -22,6 +29,7 @@ import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import javafx.util.Callback;
 
 /**
  *
@@ -29,16 +37,26 @@ import javafx.stage.Stage;
  */
 public class CanteenAccountManager extends Application {
 
-    Image imgBora = new Image("img//programmer_vs_hacker.jpg");
-    Image imgKaan = new Image("img//chemistry-cat.jpg");
-    Image imgDebtButton = new Image("img//debt_icon.jpg");
-    Button debtButton = new Button();
+    private final Image imgBora = new Image("img//programmer_vs_hacker.jpg");
+    private final Image imgKaan = new Image("img//chemistry-cat.jpg");
+    private final Image imgDebtButton = new Image("img//debt_icon.jpg");
+    private final Image backPage = new Image("img//back_icon.png");
+    private final Button debtButton = new Button();
+
+    /**
+     *
+     */
+    public final static ObservableList account_ObservableList = FXCollections.observableArrayList();
+    public final static ObservableList debts_ObservableList = FXCollections.observableArrayList();
+    private ListView debts_listView = new ListView();
+
+    
         
     @Override
     public void start(Stage stage) {
         
         Scene scene = DebtPageDesign();
-        //scene.getStylesheets().add("canteenaccountmanager//Design.css");
+        scene.getStylesheets().add("canteenaccountmanager//Design.css");
         stage.setTitle("Canteen Account Manager");
         stage.setScene(scene);
         stage.show();
@@ -98,26 +116,82 @@ public class CanteenAccountManager extends Application {
         BorderPane root = new BorderPane();
         root.setCenter(vbox);
         
-        Scene S1 = new Scene(root, 600, 500);
-        return S1;
+        Scene scene = new Scene(root, 600, 500);
+        return scene;
     }
     
     private Scene DebtPageDesign()  {
+    	ImageView back = new ImageView(backPage);
+    	back.setFitHeight(50);
+    	back.setFitWidth(50);
+    	Button backPageButton = new Button(null, back);
+    	
+    	Label title = new Label("Hesap Listesi");
+    	title.fontProperty().setValue(Font.font("Arial", FontWeight.BOLD, 24));
+    	title.setTextFill(Color.BLUE);
+    	
+    	GridPane gridTop = new GridPane();
+    	gridTop.add(backPageButton, 0, 0);
+    	gridTop.add(title, 1, 0);
+    	gridTop.setPadding(new Insets(10, 5, 10, 5));
+    	gridTop.setHgap(10);
+    	title.setAlignment(Pos.CENTER_RIGHT);
+    	backPageButton.setAlignment(Pos.BASELINE_LEFT);
         
-        Image backPage = new Image("img//arrow-back-icon(1).png");
-        ImageView backPageView = new ImageView(backPage);
+        /******
+        *
+        *   ListView of the Accounts
+        * 
+        ******/        
         
-        Label title = new Label("Hesap Listesi");
+        String[] test_array = new String[20];
+        test_array[0] = "Evrim";
+        test_array[1] = "Eda Abla";
+        test_array[2] = "Tarık Bey";
         
-        FlowPane top = new FlowPane(backPageView, title);
-        top.setAlignment(Pos.BASELINE_CENTER);
+        String[] test_debts = new String[20];
+        test_debts[0] = "20.50";
+        test_debts[1] = "74.25";
+        test_debts[2] = "64.00";
         
+        ListView<String> accounts_listView = new ListView<>();
+        accounts_listView.setEditable(true);
+        
+        account_ObservableList.addAll(test_array);
+        accounts_listView.setItems(account_ObservableList);
+        
+        debts_ObservableList.addAll(test_debts);
+        debts_listView.setItems(debts_ObservableList);
+        
+        debts_listView.setCellFactory(new Callback<ListView,
+                ListCell>(){
+                @Override
+                public ListCell<String> call(ListView p) {
+                    return new TextFieldRectCell();
+                } 
+            });
+        HBox box = new HBox(accounts_listView, debts_listView);
+
         BorderPane root = new BorderPane();
-        root.setTop(top);
-        
-        
-        Scene scene = new Scene(root, 600, 500);
+        root.setTop(gridTop);
+        root.setCenter(box);
+        Scene scene = new Scene(root, 500, 500);
         return scene;
+    }
+
+    //Özge Camel-Stationary Travaller
+    
+    private static class TextFieldRectCell extends ListCell<String> {
+
+        @Override
+        public void updateItem(String item, boolean empty)  {
+            super.updateItem(item, empty);
+                TextField tf = new TextField();
+                if(item != null)   {
+                tf.setText(item);
+                    setGraphic(tf);
+            }
+        }                
     }
 }
 
